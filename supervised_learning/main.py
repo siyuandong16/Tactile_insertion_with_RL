f2ffa2209ab39c9a7011dc55de63a71cbbac82db
siyuandong16 <siyuandong.bme@gmail.com>
2020-01-17 18:04:51 -0500

    add supervised learning code

---
+++ supervised_learning/main.py
@@ -0,0 +1,131 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
+os.environ["CUDA_VISIBLE_DEVICES"]='0,1,2,3,4'
+import sys
+
+import numpy as np 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import torchvision
+from torchvision import datasets, transforms
+
+from utils import CNN_Actor, data_selection
+
+
+def run(init_lr=0.0001, max_epoch=64e3, batch_size=128*10, save_model=''):
+
+    use_color = True 
+    train_set, valid_set, train_data_size, valid_data_size = data_selection(use_color)
+    print('training data size: ', train_data_size, 'validation data size: ', valid_data_size)
+    params = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 4, 'pin_memory': True} 
+    train_loader = torch.utils.data.DataLoader(train_set, **params)
+    valid_loader = torch.utils.data.DataLoader(valid_set, **params)
+
+    dataloaders = {'train': train_loader, 'val': valid_loader}
+  
+    model = CNN_Actor(num_inputs = 8*2*3)
+    # print(model)
+    model.cuda()
+    model = nn.DataParallel(model)
+
+    lr = init_lr
+    # optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=0.0000001)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    lr_sched = optim.lr_scheduler.MultiStepLR(optimizer, [300, 1000])
+    loss_function = nn.MSELoss() 
+
+    num_steps_per_update = 1 # accum gradient
+    epoch = 0
+    gap = 1
+    train_error_list = []
+    valid_error_list = [1.]
+    smallest_valid_error = 1.
+
+    # train it
+    while epoch < max_epoch:#for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, max_epoch))
+        print('-' * 10)
+        steps = 0
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()  # Set model to evaluate mode
+                
+            tot_loss = 0.0
+            tot_loc_loss = 0.0
+            tot_cls_loss = 0.0
+            num_iter = 0
+            acc_old= 0.
+            sample_number = 0
+            optimizer.zero_grad()
+
+            # Iterate over data.
+            for (X, y) in dataloaders[phase]:
+                num_iter += 1
+                steps += 1 
+                #print(X.size(),y.size())
+                # get the inputs
+                # inputs, labels = data
+                # inputs_1_temp,labels = X,y
+                # inputs_1 = torch.from_numpy(np.array(inputs_1)).type(torch.FloatTensor)
+                # inputs_2 = torch.from_numpy(np.array(inputs_2)).type(torch.FloatTensor)
+                # wrap them in Variable
+                # print inputs.shape 
+                inputs = Variable(X.cuda())
+                labels = Variable(y.cuda())
+
+                error_predicted = model(inputs)
+                # print 'predict_error',per_frame_logits.squeeze().size()
+                # print 'gt error',labels.size()
+                cls_loss = F.smooth_l1_loss(error_predicted.squeeze(),labels)
+                sample_number += y.size(0)
+
+
+                # print cls_loss.size()
+                tot_cls_loss += cls_loss.data
+
+                loss = (cls_loss)/num_steps_per_update
+                #loss = (0.5*loc_loss + 0.5*cls_loss)/num_steps_per_update
+
+                tot_loss += loss.data
+                loss.backward()
+             
+                if num_iter == num_steps_per_update and phase == 'train':
+                    num_iter = 0
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    lr_sched.step()
+                    # print steps%gap 
+                    if steps % gap == 0:
+                        print('{} {:.4f} Cls Loss: {:.4f} Tot Loss: {:.4f}'.format(phase, \
+                            sample_number*1.0/train_data_size,tot_cls_loss/(gap*num_steps_per_update), tot_loss/gap))
+                        # save model
+                        train_error_list.append([epoch,tot_loss/gap])
+                        np.save(save_model+'train_error_list.npy',train_error_list)
+                        # torch.save(model.module.state_dict(), save_model+str(steps).zfill(6)+'.pt')
+                        tot_loss = tot_cls_loss = 0.
+                        # label_true_list = []
+                        # label_predict_list = []
+
+
+            if phase == 'val':
+                print('{} Cls Loss: {:.4f} Tot Loss: {:.4f}'.format(phase, tot_cls_loss/num_iter, \
+                    (tot_loss*num_steps_per_update)/num_iter)) 
+                if tot_cls_loss/num_iter < smallest_valid_error:
+                    torch.save(model.state_dict(), save_model+'best_model_color_small_decrease_bnorm.pt')
+                    smallest_valid_error = tot_cls_loss/num_iter
+                valid_error_list.append(tot_cls_loss/num_iter)
+                np.save(save_model+'valid_error_list.npy',valid_error_list)
+            epoch += 1
+
+
+
+if __name__ == '__main__':
+    # need to add argparse
+    run(save_model='/home/ubuntu/packing/weights/')
---
+++ supervised_learning/utils.py
@@ -0,0 +1,153 @@
+import numpy as np 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from random import shuffle 
+import random 
+from torch.utils import data
+import cv2
+import scipy.misc 
+import os 
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.33, 0.33, 0.34])
+
+def init(module, weight_init, bias_init, gain=1):
+    weight_init(module.weight.data, gain=gain)
+    bias_init(module.bias.data)
+    return module
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class CNN_Actor(nn.Module):
+    def __init__(self, num_inputs = 8, hidden_size=256, num_classes = 3):
+        super(CNN_Actor, self).__init__()
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
+                               constant_(x, 0), nn.init.calculate_gain('relu'))
+
+        self.main = nn.Sequential(
+            init_(nn.Conv2d(num_inputs, 64, 7, stride=4)), nn.ReLU(),
+            init_(nn.Conv2d(64, 32, 5, stride=2)), nn.ReLU(), 
+            init_(nn.Conv2d(32, 32, 3, stride=1)), nn.ReLU(), Flatten(),
+            # init_(nn.Linear(32 * 10 * 10, hidden_size)), nn.ReLU()
+            init_(nn.Linear(32 * 6 * 6, hidden_size)), nn.ReLU()
+            )
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
+                               constant_(x, 0))#, nn.init.calculate_gain('tanh'))
+
+        self.critic_linear = init_(nn.Linear(hidden_size, num_classes))
+
+
+    def forward(self, inputs):
+        x = self.main(inputs)
+        x = self.critic_linear(x)
+        x = torch.tanh(x)
+        return x
+
+
+
+class Dataset_CRNN(data.Dataset):
+    "Characterizes a dataset for PyTorch"
+    def __init__(self, root, file_folder, use_color):
+        "Initialization"
+        self.root = root 
+        self.file_folder = file_folder
+        self.use_color = use_color
+        
+    def __len__(self):
+        "Denotes the total number of samples"
+        return len(self.file_folder)
+
+    def label_correction(self, label):
+        
+        label[:2] /= 10.
+        label[2] /= 15.
+
+        return label 
+
+    def load_data(self, data_path):
+        img2_seq = []
+        range_list = list(range(0, 45, 6)) + list(range(45, 90, 6))
+        for i in range_list:
+            img = cv2.imread(data_path+str(i)+'.jpg')
+            img = img[30:-30, 30:-30, :]
+            if not self.use_color:
+                imgwc_gray = rgb2gray(img)
+            else:
+                imgwc_gray = np.array(img).astype(np.float32)
+            img2_temp = scipy.misc.imresize(imgwc_gray,(84,84))
+            if i == 0 or i == 45:  
+                mean_2 = np.mean(img2_temp)
+                std_2 = np.std(img2_temp)
+
+            img2_temp = (img2_temp-mean_2)/std_2
+            if not self.use_color:
+                img2_seq.append(img2_temp)   
+            else:
+                if i == 0: 
+                    img2_seq = img2_temp.copy()
+                    img2_seq = img2_seq.transpose(2,0,1)
+                else: 
+                    img2_seq = np.concatenate((img2_seq, img2_temp.transpose(2,0,1)), axis=0)
+        img2_temp = np.array(img2_seq)
+        X = torch.from_numpy(img2_temp).type(torch.FloatTensor)  
+
+        label = np.array(np.load(data_path + 'label_true.npy'))  
+        label = self.label_correction(label)
+        Y = torch.from_numpy(label).type(torch.FloatTensor)
+        return X, Y 
+
+    def __getitem__(self, index):
+        "Generates one sample of data"
+        # Select sample
+        filename = self.file_folder[index]
+        # Load data
+        X, Y = self.load_data(filename)                 
+        return X, Y
+
+
+
+def data_selection(use_color):
+    root = []
+    root.append("/home/ubuntu/packing/data/data_newsensor_7/")
+    root.append("/home/ubuntu/packing/data/data_newsensor_8/")
+
+    num_data = 3000 
+
+    file_folder = []  
+
+    range_list = list(range(0, 45, 6)) + list(range(45, 90, 6))
+    for k in range(len(root)):
+        root_folder = root[k]
+        for i in range(num_data):
+            path = root_folder+str(i)+'/'
+            break_sign = False
+            if os.path.isdir(path):
+                for j in range_list:
+                    img = cv2.imread(path+str(j)+'.jpg')
+                    if img is None:
+                        break_sign = True
+                        break
+                label = np.array(np.load(path + 'label_true.npy'))  
+                if not (label[0] > 0 and label[1] < 0) and (not break_sign):
+                    if (label[0] < 0 and label[1] < 1.0) or (label[0] > -1 and label[1] > 0):
+                        file_folder.append(path)
+
+    num_of_data = len(file_folder)
+    num_of_train = int(num_of_data*0.8) 
+    num_of_valid = num_of_data - num_of_train 
+
+    random.seed(40)
+    shuffle(file_folder)
+
+    train_folder = file_folder[:num_of_train]
+    valid_folder = file_folder[num_of_train:]
+
+    train_set, valid_set = Dataset_CRNN(root, train_folder, use_color), Dataset_CRNN(root, valid_folder, use_color)
+
+    return train_set, valid_set, num_of_train, num_of_valid
+
